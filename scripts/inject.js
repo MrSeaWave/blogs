0bef00b98e6ebe2eae24cdf7c20f32dc49e6d638
MrSeaWave <MrDaemon@outlook.com>
2023-02-02 15:14:04 +0800

    chore: add sitemap

---
+++ scripts/inject.js
@@ -0,0 +1,7 @@
+// https://github.com/ppoffice/hexo-theme-icarus/issues/1009
+
+// google-site-verification
+hexo.extend.injector.register(
+  'head_begin',
+  '<meta name="google-site-verification" content="TBdWfG8iKkd2DhrAEq6qNuJT10KjlP-jQLE3TWq7osk" />'
+);
