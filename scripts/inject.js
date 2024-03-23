f0c54d38426710c34f9ec8d3e4f52481b61c4218
MrSeaWave <MrDaemon@outlook.com>
2024-03-23 15:02:40 +0800

    chore: add google hailangya seo

--- scripts/inject.js
+++ scripts/inject.js
@@ -5,3 +5,9 @@ hexo.extend.injector.register(
   'head_begin',
   '<meta name="google-site-verification" content="TBdWfG8iKkd2DhrAEq6qNuJT10KjlP-jQLE3TWq7osk" />'
 );
+
+// https://hailangya.com/ google seo
+hexo.extend.injector.register(
+  'head_begin',
+  '<meta name="google-site-verification" content="R0Too8dtf3BOLeRcQuJernRnUiOqxtPwP-RpfVIGdN4" />'
+);
